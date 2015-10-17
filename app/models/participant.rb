105ae186364fcfcde6ac18232fdb4ba798973a0b
James Lieu <j.lieu888@gmail.com>
2015-10-18 00:10:46 +0100

    Add total participants column to table

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,4 +3,5 @@ class Participant < ActiveRecord::Base
   belongs_to :activity
 
   default_scope { joins(:activity).order("activities.scheduled_at asc") }
+  scope :attending, -> { where(attending: true) }
 end
